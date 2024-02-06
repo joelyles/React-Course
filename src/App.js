7dd1018a5d49678a609295bcfa0ecebb322255c2
Joe Lyles <lylesjoe@protonmail.com>
2024-02-06 05:55:40 -0500

    jsx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+// component
 function App() {
+
+  {/* (array) objects will not render, boolean will not render */}
+
+  const name = 'Joe';
+  const myArray = [{name: 'joe', hobby: 'coding'}, {name: 'andrew', hobby: 'soccer'}];
+
+{/* functions should be named with "handle" */}
+
+  const handleNameChange = () => {
+    const names = ['bilbo', 'andrew', 'joe'];
+    const int = Math.floor(Math.random() * names.length);
+    return names[int];
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to see change.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className='paragraph'>Hello {handleNameChange()}</p>
       </header>
     </div>
   );
