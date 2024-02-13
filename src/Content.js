cc58aba3213594ee27b66ea337217ab2f9891d97
Joe Lyles <lylesjoe@protonmail.com>
2024-02-13 06:04:10 -0500

    list and line item components

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,4 @@
-// icons inporded from https://www.npmjs.com/package/react-icons
-import { FaTrashAlt } from 'react-icons/fa'
+import ItemList from './ItemList'
 
 const Content = ({ items, handleCheck, handleDelete}) => {
 
@@ -7,29 +6,14 @@ const Content = ({ items, handleCheck, handleDelete}) => {
     return (
       <main>
         {items.length ? (
-          <ul>
-            {items.map((item) => (
-              <li className='item' key={item.id}>
-                <input 
-                  type="checkbox" 
-                  onChange={() => handleCheck(item.id)}
-                  checked={item.checked} 
-                />
-                <label
-                  style={(item.checked) ? { textDecoration: 'line-through'} : null}
-                  onDoubleClick={() => handleCheck(item.id)}
-                >{item.item}</label>
-                <FaTrashAlt 
-                  onClick={() => handleDelete(item.id)}
-                  role="button" 
-                  tabIndex="0" 
-                />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p style={{ marginTop: '2rem'}}>your list is empty</p>
-        )} 
+          <ItemList
+            items={items}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
+          ) : (
+            <p style={{ marginTop: '2rem'}}>your list is empty</p>
+          )} 
       </main>
     )
 };
---
+++ src/LineItem.js
@@ -0,0 +1,30 @@
+// icons inporded from https://www.npmjs.com/package/react-icons
+import { FaTrashAlt } from 'react-icons/fa'
+
+const ItemList = ({item, handleCheck, handleDelete}) => {
+    return (
+     
+        
+            <li className='item' key={item.id}>
+            <input 
+                type="checkbox" 
+                onChange={() => handleCheck(item.id)}
+                checked={item.checked} 
+            />
+            <label
+                style={(item.checked) ? { textDecoration: 'line-through'} : null}
+                onDoubleClick={() => handleCheck(item.id)}
+            >{item.item}</label>
+            <FaTrashAlt 
+                onClick={() => handleDelete(item.id)}
+                role="button" 
+                tabIndex="0" 
+                aria-label={'Delete ${item.item}'}
+            />
+            </li>
+   
+       
+    )
+}
+
+export default ItemList
